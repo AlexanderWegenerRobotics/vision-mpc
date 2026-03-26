82a02764faab3c03923e6eb45e0d09b3770423f6
Alex Wegener <alexander_wegener1998@yahoo.de>
2026-03-25 21:03:23 -0400

    fixed arm model

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from simcore import RobotSystem, Pose, load_yaml
 
-cfg = load_yaml("configs/global_config.yaml")
-system = RobotSystem(cfg)
-
+config = load_yaml("configs/global_config.yaml")
+system = RobotSystem(config)
 system.set_controller_mode("arm", "impedance")
-target = Pose(position=[0.5, 0.0, 0.8], quaternion=[0, 1, 0, 0])
+target = Pose(position=[0.46, -0.07, 0.52], quaternion=[0, 1, 0, 0])
 system.set_target("arm", {"x": target})
 
 system.run()
